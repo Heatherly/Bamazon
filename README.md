e76e5e567bc55376361ebd4b2749eaab866bb898
Heather Sosa <heatherlsosa@gmail.com>
2017-08-06 10:41:57 -0600

    Testing README.md formatting, add more prodcuts

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Bamazon
 Amazon-style CLI (Command Line Interface) using Node.js, JavaScript and MySQL
+
+In this CLI App, customers can shop for a variety of products offered by Bamazon. Store managers can also manage their inventory. Let's begin with the customer view.
+
+Before you run the app, make sure you have installed the required Node modules first. You can do this be running <npm install> in your Command Terminal. Also, start the MySQL server.
+
+##Customer View##
+To begin, enter into your Terminal `node bamazonCustomer`. 
+
+![Starting the Program](/images/bamCustomer_01.PNG)
+Format: ![Starting the Customer View]
--- bamazonDB.sql
+++ bamazonDB.sql
@@ -7,9 +7,9 @@ CREATE TABLE products(
   item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NOT NULL,
   department_name VARCHAR(100) NOT NULL,
-  price DECIMAL (5,2),
+  price DECIMAL (6,2),
   stock_quantity INT,
   PRIMARY KEY (item_id)
 );
 
-INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ("Coffee mug", "Housewares", 5.00, 25), ("T-shirt", "Men Clothing", 10.50, 10);
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ("Coffee Mug", "Housewares", 5.00, 25),("Coffee Maker", "Housewares", 45.00, 15),("T-shirt", "Men Clothing", 10.50, 10),("Movie-logo Tie", "Men's Clothing", 19.50, 8),("Dress", "Women's Clothing", 35.00, 12),("Lawn Mower", "Home & Garden", 300.00, 5),("Weed Trimmer", "Home & Garden", 150.00, 7), ("60-inch TV", "Electronics", 600.00, 10), ("Blu-ray Player", "Electronics", 100.00, 15), ("MacBook Pro", "Electronics", 1600.00, 4);
